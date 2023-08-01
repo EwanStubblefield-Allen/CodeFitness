93066cf1c4e6407f21068f8b71cd842d1e742ec1
Faith <fthmorgan4@gmail.com>
2023-08-01 13:18:33 -0600

    commit

--- CodeFitness/server/controllers/RoutinesController.js
+++ CodeFitness/server/controllers/RoutinesController.js
@@ -10,11 +10,11 @@ export class RoutinesController extends BaseController {
       .get('/:routineId', this.getRoutineById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createRoutine)
-    // .delete('/:routineId', this.removeRoutine)
+      .delete('/:routineId', this.removeRoutine)
   }
   async getRoutineById(req, res, next) {
     try {
-      const routineId = req.params.id
+      const routineId = req.params.routineId
       const routine = await routinesService.getRoutineById(routineId)
 
       return res.send(routine)
@@ -22,16 +22,16 @@ export class RoutinesController extends BaseController {
       next(error)
     }
   }
-  // async removeRoutine(req, res, next) {
-  //   try {
-  //     const accountId = req.userInfo.id
-  //     const routineId = req.params.id
-  //     const routine = await routinesService.removeRoutine(accountId, routineId)
-  //     return res.send(routine)
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
+  async removeRoutine(req, res, next) {
+    try {
+      const accountId = req.userInfo.id
+      const routineId = req.params.routineId
+      const routine = await routinesService.removeRoutine(accountId, routineId)
+      return res.send(routine)
+    } catch (error) {
+      next(error)
+    }
+  }
   async getRoutines(req, res, next) {
     try {
       const routines = await routinesService.getRoutines()
@@ -44,8 +44,8 @@ export class RoutinesController extends BaseController {
   async createRoutine(req, res, next) {
     try {
       const routineData = req.body
-      debugger
-      
+
+
       routineData.creatorId = req.userInfo.id
       const routine = await routinesService.createRoutine(routineData)
       return res.send(routine)
--- CodeFitness/server/services/RoutinesService.js
+++ CodeFitness/server/services/RoutinesService.js
@@ -1,13 +1,20 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 class RoutinesService {
   async getRoutineById(routineId) {
     const routine = await dbContext.Routines.findById(routineId)
     return routine
   }
-  // removeRoutine(accountId, routineId) {
-  //   const routineToRemove
-  // }
+  async removeRoutine(accountId, routineId) {
+    const routineToRemove = await this.getRoutineById(routineId)
+    if (routineToRemove.accountId.toString() != accountId) {
+      throw new Forbidden('You are not the creator!')
+    }
+    routineToRemove.public = false
+    await routineToRemove.save()
+    return routineToRemove
+  }
   async getRoutines() {
     const routines = await dbContext.Routines.find()
     return routines
