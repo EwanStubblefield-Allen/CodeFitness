55ac23f7d792b049a187efa8e2368455b8e6e590
Faith <fthmorgan4@gmail.com>
2023-08-01 13:02:59 -0600

    commit

--- CodeFitness/server/controllers/RoutinesController.js
+++ CodeFitness/server/controllers/RoutinesController.js
@@ -7,18 +7,40 @@ export class RoutinesController extends BaseController {
     super('api/routines')
     this.router
       .get('', this.getRoutines)
+      .get('/:routineId', this.getRoutineById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createRoutine)
+    // .delete('/:routineId', this.removeRoutine)
   }
-  async getRoutines (req, res, next) {
-  try{
+  async getRoutineById(req, res, next) {
+    try {
+      const routineId = req.params.id
+      const routine = await routinesService.getRoutineById(routineId)
+
+      return res.send(routine)
+    } catch (error) {
+      next(error)
+    }
+  }
+  // async removeRoutine(req, res, next) {
+  //   try {
+  //     const accountId = req.userInfo.id
+  //     const routineId = req.params.id
+  //     const routine = await routinesService.removeRoutine(accountId, routineId)
+  //     return res.send(routine)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+  async getRoutines(req, res, next) {
+    try {
       const routines = await routinesService.getRoutines()
-  return res.send(routines)
-  } catch(error) {
+      return res.send(routines)
+    } catch (error) {
       next(error);
+    }
   }
-  }
-  
+
   async createRoutine(req, res, next) {
     try {
       const routineData = req.body
--- CodeFitness/server/services/RoutinesService.js
+++ CodeFitness/server/services/RoutinesService.js
@@ -1,9 +1,16 @@
 import { dbContext } from "../db/DbContext.js"
 
 class RoutinesService {
+  async getRoutineById(routineId) {
+    const routine = await dbContext.Routines.findById(routineId)
+    return routine
+  }
+  // removeRoutine(accountId, routineId) {
+  //   const routineToRemove
+  // }
   async getRoutines() {
-const routines = await dbContext.Routines.find()
-return routines
+    const routines = await dbContext.Routines.find()
+    return routines
   }
   async createRoutine(routineData) {
     const routine = await dbContext.Routines.create(routineData)
