75cebcdf1a69e37c6cc325d95ff9ce7ffdbb78f9
Jasper Delight <jasperdelightt@gmail.com>
2023-08-01 22:24:51 -0600

    create/get accountAchievements moved

--- CodeFitness/server/controllers/AccountAchievementsController.js
+++ CodeFitness/server/controllers/AccountAchievementsController.js
@@ -6,24 +6,15 @@ export class AccountAchievementsController extends BaseController {
   constructor() {
     super('api/accountachievements')
     this.router
-      .get('', this.getAccountAchievements)
-      .use(Auth0Provider.getAuthorizedUserInfo)
-      .post('', this.createAccountAchievement)
+    .get('', this.getAccountAchievements)
+
   }
+
+  
   async getAccountAchievements(req, res, next) {
     try {
       const accountAchievement = await accountAchievementsService.getAccountAchievement()
       return res.send(accountAchievement)
-    } catch (error) {
-      next(error);
-    }
-  }
-  async createAccountAchievement(req, res, next) {
-    try {
-      const data = req.body
-      data.accountId = req.userInfo.id
-      const newAccountAchievement = await accountAchievementsService.createAccountAchievement(data)
-      return res.send(newAccountAchievement)
     } catch (error) {
       next(error);
     }
