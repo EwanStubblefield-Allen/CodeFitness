ebe7f73df3d61500759fa2e825d4cc293db7efc4
Faith <fthmorgan4@gmail.com>
2023-08-03 09:19:09 -0600

    commit

--- CodeFitness.client/src/components/TeamComponent.vue
+++ CodeFitness.client/src/components/TeamComponent.vue
@@ -1,3 +1,59 @@
+<template>
+  <div @click="selectCommunityOne()" class="col-2 bg-neutral-light my-4 p-3 text-center selectable">
+    <p>Cardio Kings</p>
+    <p class="pt-3">Global Points: 0</p>
+  </div>
+  <div @click="selectCommunityTwo()" class=" col-2 bg-neutral-light my-4 p-3 text-center selectable">
+    <p>Weight Warriors</p>
+    <p class="pt-3">Global Points: 0</p>
+  </div>
+  <div @click="selectCommunityThree()" class=" col-2 bg-neutral-light my-4 p-3 text-center selectable">
+    <p>Legion of Leisure</p>
+    <p class="pt-3">Global Points: 0</p>
+  </div>
+</template>
+
+<script>
+import { AppState } from '../AppState.js'
+import { computed, onMounted } from 'vue'
+import { logger } from "../utils/Logger.js"
+import Pop from "../utils/Pop.js"
+import { accountService } from "../services/AccountService.js"
+
+export default {
+  setup() {
+    onMounted(() => {
+
+    })
+
+    return {
+      async selectCommunityOne() {
+        try {
+          await accountService.selectCommunityOne()
+        } catch (error) {
+          Pop.error(error.message)
+        }
+      },
+
+      async selectCommunityTwo() {
+        try {
+          await accountService.selectCommunityTwo()
+        } catch (error) {
+          Pop.error(error.message)
+        }
+      },
+
+      async selectCommunityThree() {
+        try {
+          await accountService.selectCommunityThree()
+        } catch (error) {
+          Pop.error(error.message)
+        }
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
 <style lang="scss" scoped></style>
