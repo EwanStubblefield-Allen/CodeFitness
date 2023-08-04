2fa35d08e0b06fa4c2dfffa957d6f09a01b9e86a
Object Oriented ProgrammingObject Oriented ProgrammingObject Oriented ProgrammingObject Oriented Programming <back>
2023-08-04 09:19:49 -0600

    Fixed create routines

--- CodeFitness.client/src/services/RoutinesService.js
+++ CodeFitness.client/src/services/RoutinesService.js
@@ -23,13 +23,13 @@ class RoutinesService {
   async getRoutineById(routineId) {
     const res = await api.get(`api/routines/${routineId}`)
     logger.log('got one routine', res.data)
-    AppState.activeRoutine = new Routine(res.data.routine)
+    AppState.activeRoutine = new Routine(res.data)
   }
 
   async createRoutine(routineData) {
     const res = await api.post('api/routines', routineData)
     logger.log(res.data)
-    AppState.routines.push(new Routine(res.data))
+    AppState.routines.push(new Routine(res.data.routine))
   }
 }
 
