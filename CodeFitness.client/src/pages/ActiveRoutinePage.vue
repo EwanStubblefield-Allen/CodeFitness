24b6ed383cc8fa29ae8df564ae52246456f382ea
Jasper Delight <jasperdelightt@gmail.com>
2023-08-04 12:01:40 -0600

    activity routine

--- CodeFitness.client/src/pages/ActiveRoutinePage.vue
+++ CodeFitness.client/src/pages/ActiveRoutinePage.vue
@@ -1,16 +1,51 @@
 <template>
-  <div class="col-10 bg-dark">
-<div class="row  text-center justify-content-between">
+  <div class="col-9 mt-2 m-auto bg-dark" style="height: 70vh;">
+<div class="row  text-center justify-content-around">
 <div class="col-12" style="height: 5vh;"></div>
   <div class="col-2 bg-light">
-    biceps
+    <section class="row">
+      <div class="col-12 my-2">Biceps</div>
+      <div class="col-12 my-2">Information</div>
+      <div class="col-12 my-2">Reps</div>
+    </section>
   </div>
   <div class="col-6 bg-light">
-    <div class="row">
+    <section class="row">
+      <div class="col-12">Triceps</div>
+      <div class="col-12 my-2">Information</div>
+      <div class="col-12 my-2">Reps</div>
+      <div class="col-12">Description
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, sit!
+        </p>
+        <div class="col-12 d-flex justify-content-between">
+          <button class="btn btn-primary">back</button>
+          <button class="btn btn-primary">next</button>
+        </div>
+      </div>
+    </section>
+  </div>
 
-    </div>Triceps</div>
-  <div class="col-2 bg-light">Shoulder</div>
+  <div class="col-2 bg-light">
+    <section class="row">
+
+      <div class="div">Shoulder</div>
+      <div class="col-12 my-2">Information</div>
+      <div class="col-12 my-2">Reps</div>
+    </section>
+  </div>
 </div>
+  </div>
+
+<div class="col-2 bg-neutral">
+
+</div>
+
+  <div class="col-9 mt-2 m-auto bg-primary" style="height: 40vh;">
+
+    <section class="row">
+<div class="col-12"></div>
+    </section>
   </div>
 
 </template>
