61fb7607f9b95bef432773d924fe3f542eb28b3d
Jasper Delight <jasperdelightt@gmail.com>
2023-08-09 15:32:53 -0600

    changed word

--- CodeFitness.client/src/services/PicturesService.js
+++ CodeFitness.client/src/services/PicturesService.js
@@ -3,7 +3,7 @@ import { pictureApi } from "./AxiosService.js"
 
 class PicturesService {
   async getPictures(name) {
-    const res = await pictureApi.get(`?key=AIzaSyAG2TXtbKXMKVwK02ThWBoFlvfFudMLTr4&cx=13d92f08b023f4a88&searchType=image&q=${name}+exercise+instruction`)
+    const res = await pictureApi.get(`?key=AIzaSyAG2TXtbKXMKVwK02ThWBoFlvfFudMLTr4&cx=13d92f08b023f4a88&searchType=image&q=${name}+exercise+diagram`)
     // logger.log(res.data.items[0].link)
     return res.data.items[0].link
     // const foundVideo = res.data.videos.find(v => v.width / v.height > 1)
