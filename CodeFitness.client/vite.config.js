39c960d3860bf5646f7d66ee9923c008452fe50f
Jasper Delight <jasperdelightt@gmail.com>
2023-08-11 11:53:43 -0600

    VueTour

--- CodeFitness.client/vite.config.js
+++ CodeFitness.client/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: { entries: ["vue3-tour"]},
   build: {
     outDir: 'docs',
     sourcemap: false
